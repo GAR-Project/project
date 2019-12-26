387f67d2d536d71f4b190fb802d5d93a9d53480e
Pablo Collado <pcolladosoto@gmail.com>
2019-12-26 20:35:14 +0100

    AI template ready!

---
+++ src/traffic_classifier.py
@@ -0,0 +1,21 @@
+class traffic_classifier:
+    def __init__():
+        # Include any config variables you may need
+        self.n_samples, self.traffic_mean, self.traffic_sensity = 0, 0, 0
+    
+    def work_time():
+        # Classify the traffic calling any needed methods
+        while True:
+            pass
+    
+    def get_data():
+        # Get data from InfluxDB
+        return read_traffic
+
+    def set_alarm():
+        # Ring the alarm by writing to InfluxDB
+        return
+
+if __name__ == "__main__":
+    ai_bot = traffic_classifier();
+    ai_bot.work_time()
