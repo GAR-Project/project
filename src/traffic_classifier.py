af99aed0ecef671e358c82064032a1ca9b34b191
Pablo Collado <pcolladosoto@gmail.com>
2019-12-26 20:41:42 +0100

    Minor tweaks

--- src/traffic_classifier.py
+++ src/traffic_classifier.py
@@ -1,4 +1,4 @@
-class traffic_classifier:
+class gar_py:
     def __init__():
         # Include any config variables you may need
         self.n_samples, self.traffic_mean, self.traffic_sensity = 0, 0, 0
@@ -12,10 +12,10 @@ class traffic_classifier:
         # Get data from InfluxDB
         return read_traffic
 
-    def set_alarm():
+    def ring_the_alarm():
         # Ring the alarm by writing to InfluxDB
         return
 
 if __name__ == "__main__":
-    ai_bot = traffic_classifier();
+    ai_bot = gar_py();
     ai_bot.work_time()
