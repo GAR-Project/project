451127a0cafbd4b7d39df211a377ff25239d1a1f
Pablo Collado <pcolladosoto@gmail.com>
2019-12-26 20:46:00 +0100

    Changed from spaces -> tabs!

--- src/traffic_classifier.py
+++ src/traffic_classifier.py
@@ -1,21 +1,21 @@
 class gar_py:
-    def __init__():
-        # Include any config variables you may need
-        self.n_samples, self.traffic_mean, self.traffic_sensity = 0, 0, 0
-    
-    def work_time():
-        # Classify the traffic calling any needed methods
-        while True:
-            pass
-    
-    def get_data():
-        # Get data from InfluxDB
-        return read_traffic
+	def __init__():
+		# Include any config variables you may need
+		self.n_samples, self.traffic_mean, self.traffic_sensity = 0, 0, 0
+	
+	def work_time():
+		# Classify the traffic calling any needed methods
+		while True:
+			pass
+	
+	def get_data():
+		# Get data from InfluxDB
+		return read_traffic
 
-    def ring_the_alarm():
-        # Ring the alarm by writing to InfluxDB
-        return
+	def ring_the_alarm():
+		# Ring the alarm by writing to InfluxDB
+		return
 
 if __name__ == "__main__":
-    ai_bot = gar_py();
-    ai_bot.work_time()
+	ai_bot = gar_py();
+	ai_bot.work_time()
