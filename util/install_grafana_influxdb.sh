86d5a1519096a2d413bb41c48d46c46762b0464d
davidcawork <davidcawork@gmail.com>
2019-12-28 10:47:35 +0100

    Added grafana dep

--- util/install_grafana_influxdb.sh
+++ util/install_grafana_influxdb.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 #Grafana
+sudo apt-get -fy install && apt-get install -y libfontconfig1
 wget https://dl.grafana.com/oss/release/grafana_6.5.2_amd64.deb
 sudo dpkg -i grafana_6.5.2_amd64.deb
 rm grafana_6.5.2_amd64.deb
