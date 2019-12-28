74882bb9ef455289af3752f9251cddc9af04e856
davidcawork <davidcawork@gmail.com>
2019-12-28 10:25:18 +0100

    Clean up *.deb files

--- util/install_grafana_influxdb.sh
+++ util/install_grafana_influxdb.sh
@@ -3,7 +3,9 @@
 #Grafana
 wget https://dl.grafana.com/oss/release/grafana_6.5.2_amd64.deb
 sudo dpkg -i grafana_6.5.2_amd64.deb
+rm grafana_6.5.2_amd64.deb
 
 #InfluxDB
 wget https://dl.influxdata.com/influxdb/releases/influxdb_1.7.9_amd64.deb
 sudo dpkg -i influxdb_1.7.9_amd64.deb
+rm influxdb_1.7.9_amd64.deb
--- util/install_telegraf.sh
+++ util/install_telegraf.sh
@@ -3,3 +3,4 @@
 # Telegraf
 wget https://dl.influxdata.com/telegraf/releases/telegraf_1.13.0-1_amd64.deb
 sudo dpkg -i telegraf_1.13.0-1_amd64.deb
+rm telegraf_1.13.0-1_amd64.deb
