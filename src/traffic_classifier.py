6408a49fe815160851f0525809ca261578871b5c
davidcawork <davidcawork@gmail.com>
2019-12-30 17:03:54 +0100

    Added Query intf to InfluxDB

--- src/traffic_classifier.py
+++ src/traffic_classifier.py
@@ -11,15 +11,23 @@ class gar_py:
 		self.port = port
 		self.dbname = 'telegraf'
 		self.client = InfluxDBClient(host, port, 'root', 'root', self.dbname)
-
-	
+		
+		# Comillas triples ya que si o si tenemos que usar comillas simples para taggear la etiqueta. 
+		self.query = """select bytes_sent from net where interface = 's1-eth1';"""
+		
+		# Ejemplos de querys:
+		# select icmp_inmsgs, icmp_outmsgs from net
+		# select * from net where time >= '2019-12-31T00:00:00Z' and time < '2019-12-30T16:00:00Z'
+		
 	def work_time():
 		# Classify the traffic calling any needed methods
 		while True:
 			pass
 	
-	def get_data():
+
+	def get_data(self, pet):
 		# Get data from InfluxDB
+		read_traffic = self.client.query(pet)
 		return read_traffic
 
 	def ring_the_alarm(self, boolean_value):
@@ -28,6 +36,7 @@ class gar_py:
 		if self.client.write_points(data_json):
 			print('[OK] Data was correctly sent to InfluxDB')
 		return
+
 
 	# --- Aux Methods --- #	
 	
@@ -40,9 +49,14 @@ class gar_py:
 if __name__ == "__main__":
 	ai_bot = gar_py()
 	
-	# Unit test
+	# Unit test (DDoS alarm intf)
 	print(ai_bot.get_datetime())
 	print(ai_bot.get_ddos_json_body(True))
 	ai_bot.ring_the_alarm(True)
-		
+
+	# Unit test (Query intf)
+	print('Enviando esta query: ' + ai_bot.query)
+	print('Resultado: \n' + str( ai_bot.get_data(ai_bot.query) ))	
+	
+	# Init
 	ai_bot.work_time()
