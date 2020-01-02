288225247650c4563b828b418eae03ceb68df943
Pablo Collado <pcolladosoto@gmail.com>
2020-01-02 20:41:31 +0100

    Added script for dataset preparation!

---
+++ src/data_gathering.py
@@ -0,0 +1,35 @@
+import influxdb, sys
+
+QUERY = """SELECT DERIVATIVE(icmp_inechos) AS d_ping FROM net ORDER BY time DESC LIMIT 100"""
+
+n_samples, mean = 0, 0
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        db = influxdb.InfluxDBClient('10.0.123.3', 8086, 'root', 'root', 'h4_net_stats')
+        measurement_class = sys.argv[1]
+    elif len(sys.argv) == 3:
+        db = influxdb.InfluxDBClient(sys.argv[1], 8086, 'root', 'root', 'h4_net_stats')
+        measurement_class = sys.argv[1]
+    elif len(sys.argv) == 4:
+        db = influxdb.InfluxDBClient(sys.argv[1], int(sys.argv[2]), 'root', 'root', 'h4_net_stats')
+        measurement_class = sys.argv[1]
+    elif len(sys.argv) == 5:
+        db = influxdb.InfluxDBClient(sys.argv[1], int(sys.argv[2]), 'root', 'root', sys.argv[3])
+        measurement_class = sys.argv[1]
+    else:
+        print("Usage: python3 " + sys.argv[0] + " 0 | 1 [InfluxDB_IP] [InfluxDB_port] [DB_name]")
+        exit(-1)
+
+    out_file = open("ICMP_data_class_{}.txt".format(measurement_class), "w+")
+
+    for measurement in db.query(QUERY).get_points(measurement = 'net'):
+        curr_derivative = measurement["d_ping"]
+        n_samples += 1
+        delta_mean = (curr_derivative - mean) / n_samples
+        mean += delta_mean
+        out_file.write("{}, {}, {}\n".format(curr_derivative, mean, measurement_class))
+
+    out_file.close()
+    print("Finished!")
+    exit(0)
