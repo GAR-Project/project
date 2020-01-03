64426e52f8b6b63763662a864c69bb49b2d154f5
Pablo Collado <pcolladosoto@gmail.com>
2020-01-03 02:18:40 +0100

    SVM working smooth as silk!

--- src/traffic_classifier.py
+++ src/traffic_classifier.py
@@ -5,61 +5,71 @@ import influxdb, datetime, time
 from sklearn import svm
 
 class gar_py:
-	def __init__(self, host = 'localhost', port = 8086, kern_type = 'linear'):
+	def __init__(self, db_host = 'localhost', port = 8086, db = 'h4_net_stats', kern_type = 'linear'):
 		# Constructor n.n
-		self.n_samples, self.traffic_mean = 0, 0
-		self.host = host
+		self.debug = True
+		self.n_samples, self.mean = 0, 0
+		self.host = db_host
 		self.port = port
-		self.dbname = 'h4_net_stats'
-		self.client = influxdb.InfluxDBClient(host, port, 'root', 'root', self.dbname)
+		self.dbname = db
+		self.client = influxdb.InfluxDBClient(self.host, self.port, 'root', 'root', self.dbname)
 		self.svm_inst = svm.SVC(kernel = kern_type)
+		self.training_files = ["training_datasets/ICMP_data_class_0.txt", "training_datasets/ICMP_data_class_1.txt"]
 		
 		# We need to use these triple quotes so that we can use a tag within the query! 
 		# self.query = """select bytes_sent from net where interface = 's1-eth1' order by time desc limit 1;"""
-		self.query = """SELECT DERIVATIVE(icmp_inechos) AS d_ping FROM net WHERE interface = 'h4-eth0' ORDER BY time DESC LIMIT 1"""
-		
-		# Ejemplos de querys:
-		# select icmp_inmsgs, icmp_outmsgs from net
-		# select * from net where time >= '2019-12-31T00:00:00Z' and time < '2019-12-30T16:00:00Z'
-
+		self.query = """SELECT DERIVATIVE(icmp_inechos) AS d_ping FROM net ORDER BY time DESC LIMIT 3"""
 		self.train_svm()
 
 	def train_svm(self):
 		# Dump data from a file and feed the SVM
 		features, labels = [], []
-		# Files are opened for reading and in text mode by default
-		# Secifying it doesn't hurt though!
+		# Files are opened for reading and in text mode by default. Specifying it doesn't hurt though
 		# Our file format is: ^Sample_value, Current_mean, Class$
 		# Where Class is either 0 (Not an attack) or 1 (Houston, we've got a situation!)
 		# The ^ and $ characters symbolize the beginning and end of a line like in RegExps
-		meal = open("SVM_training.txt", "rt")
-		for line in meal:
-			data_list = line.rsplit(", ")
-			# We need to work with numbers. Casting time!
-			for i in range(len(data_list)):
-				data_list[i] = int(data_list[i])
-			features.append(data_list[:2])
-			labels.append(data_list[2])
-		meal.close()
+		for fname in self.training_files:
+			meal = open(fname, "rt")
+			for line in meal:
+				data_list = line.rsplit(", ")
+				# We need to work with numbers. Casting time!
+				for i in range(len(data_list)):
+					if i < 2:
+						data_list[i] = float(data_list[i])
+					else:
+						data_list[i] = int(data_list[i])
+				features.append(data_list[:2])
+				labels.append(data_list[2])
+			meal.close()
+		if self.debug:
+			print("Features first and last entries:\n\t", end = "")
+			print(features[:1] + features[199:])
+			print("Labels first and last entries:\n\t", end = "")
+			print(labels[:1] + labels[199:])
 		self.svm_inst.fit(features, labels)
 		
 	def work_time(self):
+		last_entry_time = "0"
 		while True:
-			# Note that the get_points() method returns a generator, we need to invoke
-			# next() on it ourselves to work like this
-			new_entry = next(self.get_data(self.query).get_points(measurement = 'net'))
-			# Strings have been overloaded to allow
-			# this type comparison!
-			if new_entry['time'] > self.get_datetime():
-				n_samples += 1
-				# Take a look at the documentation for this quantity!
-				delta_mean = (new_entry['d_ping'] - mean) / (n_samples)
-				mean += delta_mean
-				if self.under_attack(new_entry['d_ping'], mean):
-					self.ring_the_alarm(True)
-			time.sleep(2)
+			# We have to reverse the elements due to how the query returns the result. We need to
+			# reverse() a list so that's why we need to cast the generator returned by get_points()
+			for new_entry in reversed(list(self.get_data(self.query).get_points(measurement = 'net'))):
+				# Strings have been overloaded to allow this type comparison!
+				if new_entry['time'] > last_entry_time:
+					last_entry_time = new_entry['time']
+					self.n_samples += 1
+					# Take a look at the documentation for this quantity!
+					delta_mean = (new_entry['d_ping'] - self.mean) / (self.n_samples)
+					self.mean += delta_mean
+					if self.debug:
+						print("\n** New entry **\n\tDelta_ICMP_inechos: " + str(new_entry['d_ping']))
+						print("\tCurrent mean: " + str(self.mean))
+					self.ring_the_alarm(self.under_attack(new_entry['d_ping'], self.mean))
+			time.sleep(5)
 	
 	def under_attack(self, ping_sample, current_mean):
+		if self.debug:
+			print("\tCurrent prediction: " + str(self.svm_inst.predict([[ping_sample, current_mean]])[0]))
 		if self.svm_inst.predict([[ping_sample, current_mean]])[0] == 1:
 			return True
 		else:
@@ -72,8 +82,8 @@ class gar_py:
 	def ring_the_alarm(self, should_i_ring):
 		# Ring the alarm by writing to InfluxDB
 		data_json = self.get_ddos_json_body(should_i_ring)
-		if self.client.write_points(data_json):
-			print('[OK] Data was correctly sent to InfluxDB :)')
+		if self.client.write_points(data_json) and self.debug:
+			print('\t[OK] Data was correctly sent to InfluxDB :)\n')
 
 	# --- Aux Methods --- #	
 	def get_ddos_json_body(self, boolean):
@@ -83,33 +93,6 @@ class gar_py:
 		return (datetime.datetime.now()).strftime('%Y-%m-%dT%H:%M:%SZ')
  
 if __name__ == "__main__":
-	ai_bot = gar_py()
-	
-	# Unit test (DDoS alarm intf)
-	print(ai_bot.get_datetime())
-	print(ai_bot.get_ddos_json_body(True))
-	ai_bot.ring_the_alarm(True)
-
-	# Unit test (Query intf)
-	print('Enviando esta query: ' + ai_bot.query)
-	print('Resultado: \n' + str( ai_bot.get_data(ai_bot.query) ))	
-	
-	# Muchos datos, como los podemos procesar?
-	data = ai_bot.get_data(ai_bot.query)
-	for measurement in data.get_points(measurement = 'net'):
-	
-		bytes_sent = measurement['bytes_sent']
-		print(str(bytes_sent))
-	
-	# tambien podemos filtrar por tag, en este caso solo estamos pidiendo de s1-eth1, pero podriamos hacer esto:
-	#
-	# get_points(tags={ 'interface': 's1-eth2'})
-	#
-	# O podemos hacer una mezcla:
-	#
-	# get_points(measurement='net',tags={ 'interface':'s2-eth1'})
-	#
-	#
-	
-	# Init
+	ai_bot = gar_py(db_host = '10.0.123.3')
+	# Load up the AI and start rocking!
 	ai_bot.work_time()
